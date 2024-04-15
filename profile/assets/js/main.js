d4eaa13f133f59a8ebd095cd1724fc7b3a10126e
TshiamoMog <110412124+TshiamoMog@users.noreply.github.com>
2024-04-15 21:48:47 +0200

    Dark Theme and Academic Records

--- profile/assets/js/main.js
+++ profile/assets/js/main.js
@@ -49,7 +49,9 @@ if(selectedTheme){
     // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
     document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme);
     themeButton.classList[selectedIcon === 'ri-moon-line' ? 'add' : 'remove'](iconTheme);
-    profileImage.src = selectedImage;
+    if(profileImage != null){
+        profileImage.src = selectedImage;
+    }
 }
 
 // Activate / Deactivate the theme manually with the button
