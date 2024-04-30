8c9202cf01f9d0bb91d6a3e22b5e43872349b1cd
TshiamoMog <110412124+TshiamoMog@users.noreply.github.com>
2024-04-30 14:51:53 +0200

    Testimonial Social

--- profile/assets/js/index.js
+++ profile/assets/js/index.js
@@ -41,3 +41,25 @@ const scrollUp = () => {
     this.scrollY >= 350 ? scrollup.classList.add('show-scroll') : scrollup.classList.remove('show-scroll')
 }
 window.addEventListener('scroll', scrollUp)
+
+/*=============== SHOW SOCIAL NETWORKS ===============*/
+const showSocial = (toggleCard, socialCard) =>{
+    const toggle = document.getElementById(toggleCard),
+          social = document.getElementById(socialCard)
+    
+    toggle.addEventListener('click', ()=>{
+        // If the animation class exists, we add the down-animation class
+        if(social.classList.contains('animation')){
+            social.classList.add('down-animation')
+
+            // We remove the down-animation class
+            setTimeout(() =>{
+                social.classList.remove('down-animation')
+            }, 1000)
+        }
+
+        // We add the animation class to the div tag with the card__social class
+        social.classList.toggle('animation')
+    })
+}
+showSocial('card-toggle','card-social')
